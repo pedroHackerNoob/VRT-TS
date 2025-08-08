7d802c7cc35c6b103230d82e8f18b72ae4004d43
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-08-07 22:50:31 -0700

    hero added

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -2,9 +2,15 @@
 export default function Home() {
     return(
         <>
-            <div className='font-black'>
-                <h1>Home main body</h1>
-                <p>This is the main body page</p>
+            <div className='grid'>
+                <h1 className='capitalize grid font-serif justify-center bg-blue-300 py-1'>Liderazgo, comunidad y acción.</h1>
+                <div className='grid bg-no-repeat h-50 md:h-100 lg:h-180 bg-cover bg-[url("https://as2.ftcdn.net/v2/jpg/12/09/29/37/1000_F_1209293731_NsTx68RoqqXpRcJsqHdJpY8Wia9lNa3c.jpg")]'>
+                    <div className='grid bg-black/60 h-full w-full'>
+                        <h1 className=' ml-2 flex items-center text-white font-bold md:text-2xl lg:text-3xl'>Bienvenido a la comunidad estudiantil.</h1>
+                    </div>
+                </div>
+                <h1 className='capitalize grid font-serif justify-center bg-blue-300 py-1'>ultimas actualizaciones.</h1>
+
             </div>
         </>
     )
